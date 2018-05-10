f69214c6a84aa8e4d8175e0cbd8184012c04fd06
Jathmel Gordon <jathmel@gmail.com>
2018-05-10 17:45:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
 # Write your code here.
+dictionary = {
+  'hello' => 'hi',
+  'to' => '2',
+  'two'=> '2',
+  'too' => '2',
+  'for' => '4',
+  'four' => '4',
+  'be' => 'b',
+  'you' => 'u',
+  'at' => '@',
+  'and' => '&'
+}
+  
+def word_substituter(tweet)
+  dictionary = {
+  'hello' => 'hi',
+  'to' => '2',
+  'two'=> '2',
+  'too' => '2',
+  'for' => '4',
+  'four' => '4',
+  'be' => 'b',
+  'you' => 'u',
+  'at' => '@',
+  'and' => '&'
+}
+  tweet.split.map do |word|
+    if dictionary.include?(word)
+      word = dictionary[word]
+    else
+      word
+    end
+  end.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  result = ''
+  tweets.each do |tweet|
+   result <<  word_substituter(tweet)
+  end
+  result
+end
+
